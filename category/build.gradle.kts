c3d50553513f8f88da8cc1382f5ad5ca1440c871
공준배 <ballgaveship@gmail.com>
2022-02-27 08:39:11 +0900

    arm64에 맞추어 docker hub에 push 할 수 있게 jib 셋업

--- category/build.gradle.kts
+++ category/build.gradle.kts
@@ -1,7 +1,38 @@
+version = "2022.0.0-SNAPSHOT"
+
+plugins {
+    id("com.google.cloud.tools.jib") version "3.2.0"
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.flywaydb:flyway-core")
     runtimeOnly("com.h2database:h2")
+}
+
+jib {
+    from {
+        image = "amazoncorretto:17"
+        platforms {
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+        }
+    }
+    to {
+        image = "ballgaveship/category-manager"
+        tags = setOf("${project.version}", "latest")
+    }
+    container {
+        ports = listOf("8080")
+        creationTime = "USE_CURRENT_TIMESTAMP"
+        jvmFlags = listOf(
+            "-XX:+UseContainerSupport",
+            "-XX:+UseStringDeduplication",
+            "-XX:+OptimizeStringConcat"
+        )
+    }
 }
